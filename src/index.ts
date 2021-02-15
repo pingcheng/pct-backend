4be630ef08d3b9984547f796433a5b397fc6067d
Ping Cheng <ping.cheng@aroflo.com>
2021-02-16 01:10:33 +1100

    add cors

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,13 @@ import postCategoriesRoutes from "./routes/posts/postCategoriesRoutes";
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get("/", (req, res) => {
     res.send(ApiResponse.with(null, "Hello from Ping Cheng"));
 });
